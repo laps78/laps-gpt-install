e6c5b2123719dd905a4df99c9254188efa0c6e50
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-06-23 01:09:34 +0300

    install.sh update

--- install.sh
+++ install.sh
@@ -18,13 +18,13 @@ echo "***********************************************************************"
 echo "L.A.P.S. GPT TELEGRAM BOT ATOMATIC INSTALLER FOR SPRINTBOX v0.1:"
 echo "init..."
 
+# install required system packages
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pip -y
+
 # create bot user
 echo "Будет создан пользователь gpt_bot. Вам будет предложено ввести и подтвердить UNIX пароль, а также заполнить дополнительную информацию о пользователе. Обязательно требуется точно ввести и повторить пароль, остальные данные - просто нажимайте enter";
 adduser gpt_bot && echo "Пользователь gpt_bot создан"
-
-# install packages
-sudo apt update && sudo apt upgrade -y
-sudo apt install python3-pip -y
 
 # switch to non-root user & configure user environment
 su - gpt_bot
