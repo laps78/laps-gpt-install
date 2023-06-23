bc978c0555ff56e6d6fd2d8d679a13a321aa5f41
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-06-23 05:15:39 +0300

    install.sh update

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ adduser gpt_bot --debug && echo "Пользователь gpt_bot создан"
 
 # switch to non-root user & configure user environment
 su - gpt_bot 
-pip3 install virtualenv --user" && echo "line 28 passed"
+pip3 install virtualenv --user && echo "line 28 passed"
 logout
 runuser -l gpt_bot -c "export PATH=$HOME/.local/bin:$PATH" && echo "line 29 passed"
 runuser -l gpt_bot -c "virtualenv --system-site-packages python" && echo "line 30 passed"
